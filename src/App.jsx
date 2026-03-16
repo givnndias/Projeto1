dbf2e9eb3559a44a75536ffc6e150df5f7903cdb
Giovanna <giovannaofdias@gmail.com>
2026-03-16 11:41:52 -0300

    feat: Criando o componente fjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,70 @@
+import { useEffect, useState} from 'react'
 import Header from './components/Header'
 import Resultado from './components/Resultado'
 
 function App() {
  
+//Hooks - useState - Manipula o estado da variavel
+//() -> Serve para declarar os parâmetros da variável
+
+const [peso, setPeso] = useState(0);
+const [altura, setAltura] = useState(0);
+const [resultado, setResultado] = useState(0);
+const [mostrarresultado, setMostrarResultado] = useState(false);
+
+// FUNÇÃO CALCULAR IMC
+
+const calcularImc = () => {
+  if(peso> 0 && altura> 0){
+    const imc = peso/ (altura * altura);
+    setResultado(imc)
+  }else{
+    alert('Insira valores válidos')
+  }
+}
+
+//EFEITO COLATERAL PARA MOSTRAR O RESULTADO
+useEffect(() => {
+  //condicional ternaria, se o resultado for maior que 0 mostra na tela 
+  resultado > 0 ? setMostrarResultado(true): setMostrarResultado(false)
+  //o efeito só executa quando a variavel for alterada 
+}, [resultado])
+
+
+
   return (
-    <>
-      <Header/>
-      <Resultado/>
-    </>
+    <section className="container">
+      <div className="box">
+        <Header/>
+        <form>
+          <div>
+            <label htmlFor="altura">Altura <span>(ex: 1.80) </span></label>
+            <input type="number" 
+            step="0.01" //permite o uso de ponto/virgula (casas decimais)
+            id="altura" 
+            placeholder="Digite sua altura"
+            onChange={({target})=>setAltura(parseFloat(target.value))}/>
+          </div>
+          <div>
+            <label htmlFor="peso">Peso <span>(ex: 45kg) </span></label>
+            <input type="number" 
+            step="0.1" //permite o uso de ponto/virgula (casas decimais)
+            id="peso" 
+            placeholder="Digite seu peso"
+            onChange={({target})=>setPeso(parseFloat(target.value))}/>
+          </div>
+          <button type="button" onClick={calcularImc}>Calcular</button>
+        </form>
+      </div>
+
+      {mostrarresultado && (
+        //Envia o valor formado com 2 casas decimais via props parao componente resultado
+        <Resultado resultado = {resultado.toFixed(2)}/>
+
+      )}
+      
+      
+    </section>
   )
 }
 
